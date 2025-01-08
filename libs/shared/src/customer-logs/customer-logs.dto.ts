9435b122507bacb55919ffe530a9e462d45c12c3
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-09 00:22:36 +0300

    fix: rm typings in customer-logs.dto.ts

--- libs/shared/src/customer-logs/customer-logs.dto.ts
+++ libs/shared/src/customer-logs/customer-logs.dto.ts
@@ -2,9 +2,9 @@ import { CustomerPreviewDTO } from "../customers/customers.dto";
 import { UserPreviewDTO } from "../users/users.dto";
 
 export interface CustomerLogDTO {
-  id: number;
+  id: string;
   action: string;
-  comment: string;
+  comment?: string;
   user: UserPreviewDTO;
   customer: CustomerPreviewDTO;
   created_at: Date;
