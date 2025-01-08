99e86af43ba16456578e5ae7ecc17736b3948448
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-08 04:30:38 +0300

    feat(shared): adds auth api

---
+++ libs/shared/src/auth/auth.api.ts
@@ -0,0 +1,21 @@
+import { APIRoutes } from "../api-routes";
+import { UserDTO } from "../users/users.dto";
+
+import { LoginDTO } from "./auth.dto";
+
+export interface LoginRequestDTO {
+  email: string;
+  password: string;
+}
+
+export interface AuthApi {
+  me(): Promise<UserDTO>;
+  login(dto: LoginRequestDTO): Promise<LoginDTO>;
+  logout(): Promise<void>;
+}
+
+export const AUTH_ROUTES: APIRoutes<AuthApi> = {
+  me: () => `/auth/me`,
+  login: () => `/auth/login`,
+  logout: () => `/auth/logout`
+};
