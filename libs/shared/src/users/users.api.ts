29ecac04a6fc597a1e5801524b19b33defb29e71
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-08 05:24:47 +0300

    update(shared): add bulkCreate method to api

--- libs/shared/src/users/users.api.ts
+++ libs/shared/src/users/users.api.ts
@@ -1,24 +1,29 @@
 import { APIRoutes } from "../api-routes";
 
-import { UserDTO, UserPreviewDTO } from "./users.dto";
+import { UserDTO, UserPreviewDTO, UserRole } from "./users.dto";
 
-export interface UserUpdateRequestDTO {
-  fullName?: string;
-  email?: string;
+export interface UserCreateRequestDTO {
+  fullName: string;
+  email: string;
   position?: string;
   avatarUrl?: string | null;
+  role: UserRole;
 }
+
+export type UserUpdateRequestDTO = Partial<Omit<UserCreateRequestDTO, "role">>;
 
 export interface UsersApi {
   findAll: () => Promise<UserPreviewDTO[]>;
   update: (userId: string, dto: UserUpdateRequestDTO) => Promise<UserDTO>;
   delete: (userId: string) => Promise<void>;
   restore: (userId: string) => Promise<void>;
+  bulkCreate: (dto: UserCreateRequestDTO) => Promise<UserPreviewDTO[]>;
 }
 
 export const USERS_ROUTES: APIRoutes<UsersApi> = {
   findAll: () => "/users",
   update: (userId: string) => `/users/${userId}`,
   delete: (userId: string) => `/users/${userId}`,
-  restore: (userId: string) => `/users/${userId}/restore`
+  restore: (userId: string) => `/users/${userId}/restore`,
+  bulkCreate: () => "/users/bulk-create"
 };
