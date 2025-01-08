dc92e9b7c4ac3ad107d2a734b4f2d3fed59e0295
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-08 05:23:54 +0300

    fix(shared): rm useless params

--- libs/shared/src/project-comments/project-comments.api.ts
+++ libs/shared/src/project-comments/project-comments.api.ts
@@ -4,16 +4,16 @@ import { ProjectCommentDTO } from "./project-comments.dto";
 
 export interface ProjectCommentsApi {
   findAll: (projectId: string) => Promise<ProjectCommentDTO[]>;
-  create: (projectId: string, text: string) => void;
-  update: (projectCommentId: string, text: string) => void;
-  delete: (projectCommentId: string) => void;
-  restore: (projectCommentId: string) => void;
+  create: (projectId: string, text: string) => Promise<void>;
+  update: (projectCommentId: string, text: string) => Promise<void>;
+  delete: (projectCommentId: string) => Promise<void>;
+  restore: (projectCommentId: string) => Promise<void>;
 }
 
 export const PROJECT_COMMENTS_ROUTES: APIRoutes<ProjectCommentsApi> = {
   findAll: (projectId: string) => `/projects/${projectId}/comments`,
-  create: (projectId: string, text: string) => `/projects/${projectId}/comments`,
-  update: (projectCommentId: string, text: string) => `/project-comments/${projectCommentId}`,
+  create: (projectId: string) => `/projects/${projectId}/comments`,
+  update: (projectCommentId: string) => `/project-comments/${projectCommentId}`,
   delete: (projectCommentId: string) => `/project-comments/${projectCommentId}`,
   restore: (projectCommentId: string) => `/project-comments/${projectCommentId}/restore`
 };
--- libs/shared/src/task-comments/task-comments.api.ts
+++ libs/shared/src/task-comments/task-comments.api.ts
@@ -4,16 +4,16 @@ import { TaskCommentDTO } from "./task-comments.dto";
 
 export interface TaskCommentsApi {
   findAll: (taskId: string) => Promise<TaskCommentDTO[]>;
-  create: (taskId: string, text: string) => void;
-  update: (taskCommentId: string, text: string) => void;
-  delete: (taskCommentId: string) => void;
-  restore: (taskCommentId: string) => void;
+  create: (taskId: string, text: string) => Promise<void>;
+  update: (taskCommentId: string, text: string) => Promise<void>;
+  delete: (taskCommentId: string) => Promise<void>;
+  restore: (taskCommentId: string) => Promise<void>;
 }
 
 export const TASK_COMMENTS_ROUTES: APIRoutes<TaskCommentsApi> = {
   findAll: (taskId: string) => `/tasks/${taskId}/comments`,
-  create: (taskId: string, text: string) => `/tasks/${taskId}/comments`,
-  update: (taskCommentId: string, text: string) => `/tasks-comments/${taskCommentId}`,
+  create: (taskId: string) => `/tasks/${taskId}/comments`,
+  update: (taskCommentId: string) => `/tasks-comments/${taskCommentId}`,
   delete: (taskCommentId: string) => `/task-comments/${taskCommentId}`,
   restore: (taskCommentId: string) => `/task-comments/${taskCommentId}/restore`
 };
