63d090cddc099e3c11ee5e35cdd4ddcbf5bf671d
Danil Kostin <d.kostin@worksolutions.ru>
2025-01-16 12:58:18 +0300

    fix(auth): fix imports

--- packages/backend/src/app/auth/auth.module.ts
+++ packages/backend/src/app/auth/auth.module.ts
@@ -1,16 +1,16 @@
+import { UsersModule } from "@backend/app/users/users.module";
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 
 import { ConfigModule } from "../config/config.module";
 import { ConfigService } from "../config/config.service";
-import { UsersService } from "../users/users.service";
 
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 
 @Module({
   imports: [
-    UsersService,
+    UsersModule,
     ConfigModule,
     JwtModule.registerAsync({
       useFactory: (configService: ConfigService) => ({
