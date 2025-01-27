d1eb269abd58442f09b3c2baf36cc2d8adf864b0
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-28 01:54:04 +0300

    feat: create log decorator

--- packages/backend/src/decorators/logger.decorator.ts
+++ packages/backend/src/decorators/logger.decorator.ts
@@ -1,1 +1,25 @@
-// TODO
+import { LoggerService } from "@backend/app/logger/logger.service";
+
+interface LoggerServiceClass {
+  loggerService: LoggerService;
+}
+
+export function Logger(action: string, comment?: string): MethodDecorator {
+  return function <T>(target: object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): void {
+    const originalMethod: T | undefined = descriptor.value;
+
+    if (typeof originalMethod !== "function") {
+      throw new Error("LoggerDecorator can only be applied to methods.");
+    }
+
+    descriptor.value = function (this: LoggerServiceClass, ...args: any[]): unknown {
+      if (!this.loggerService) {
+        throw new Error("LoggerService is not available. Ensure it is injected into the class using this decorator.");
+      }
+
+      this.loggerService.log(action, comment);
+
+      return originalMethod.apply(this, args);
+    } as T;
+  };
+}
