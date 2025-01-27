01a9927968930feba203527edd88cae053d6750c
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-28 02:34:17 +0300

    update(logger): decorator now works without di

--- packages/backend/src/decorators/logger.decorator.ts
+++ packages/backend/src/decorators/logger.decorator.ts
@@ -1,25 +1,19 @@
 import { LoggerService } from "@backend/app/logger/logger.service";
 
-interface LoggerServiceClass {
-  loggerService: LoggerService;
-}
-
 export function Logger(action: string, comment?: string): MethodDecorator {
-  return function <T>(target: object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<T>): void {
-    const originalMethod: T | undefined = descriptor.value;
+  return function (target: object, propertyKey: string | symbol, descriptor: TypedPropertyDescriptor<any>): void {
+    const originalMethod: unknown = descriptor.value;
 
     if (typeof originalMethod !== "function") {
       throw new Error("LoggerDecorator can only be applied to methods.");
     }
 
-    descriptor.value = function (this: LoggerServiceClass, ...args: any[]): unknown {
-      if (!this.loggerService) {
-        throw new Error("LoggerService is not available. Ensure it is injected into the class using this decorator.");
-      }
+    descriptor.value = function (...args: any[]): unknown {
+      const loggerService: LoggerService = LoggerService.getInstance();
 
-      this.loggerService.log(action, comment);
+      loggerService.log(action, comment);
 
-      return originalMethod.apply(this, args);
-    } as T;
+      return originalMethod.apply(this, args) as unknown;
+    };
   };
 }
