2f74d8ad13b291b77e9b7460f2d376379bbb327b
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-28 01:00:48 +0300

    fix: typo

--- packages/backend/src/database/migrations/1736126386513-create-document-permissions.ts
+++ packages/backend/src/database/migrations/1736126386513-create-document-permissions.ts
@@ -5,9 +5,9 @@ export class CreateDocumentPermissions1736126386513 implements MigrationInterfac
     await queryRunner.query(`
       CREATE TABLE "document_permissions"
       (
-        "document_permission_id" uuid             NOT NULL DEFAULT uuid_generate_v4(),
-        "document_id"            uuid             NOT NULL,
-        "role"                   "user_role_enum" NOT NULL,
+        "document_permission_id" uuid        NOT NULL DEFAULT uuid_generate_v4(),
+        "document_id"            uuid        NOT NULL,
+        "role"                   "role_enum" NOT NULL,
         CONSTRAINT "PK_document_permission_id" PRIMARY KEY ("document_permission_id"),
         CONSTRAINT "FK_document_id_document_permissions" FOREIGN KEY ("document_id") REFERENCES "documents" ("document_id") ON DELETE CASCADE,
         CONSTRAINT "UQ_document_role" UNIQUE ("document_id", "role")
