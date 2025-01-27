b7da4df20f7713aa25192414d518206cbd131048
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-27 22:17:33 +0300

    fix: type migration error

--- packages/backend/src/database/migrations/1736118954040-create-users.ts
+++ packages/backend/src/database/migrations/1736118954040-create-users.ts
@@ -3,20 +3,21 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class CreateUsers1736118954040 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-      CREATE TYPE "role_enum" AS ENUM('admin', 'user', 'moderator', 'manager);
+      CREATE TYPE "role_enum" AS ENUM('admin', 'user', 'moderator', 'manager');
     `);
 
     await queryRunner.query(`
-      CREATE TABLE "users" (
-        "user_id" uuid NOT NULL DEFAULT uuid_generate_v4(),
-        "full_name" varchar(255) NOT NULL,
-        "email" varchar(255) NOT NULL UNIQUE,
-        "password" varchar(255) NOT NULL,
-        "position" varchar(255),
-        "role" "role_enum" NOT NULL DEFAULT 'user',
+      CREATE TABLE "users"
+      (
+        "user_id"    uuid         NOT NULL DEFAULT uuid_generate_v4(),
+        "full_name"  varchar(255) NOT NULL,
+        "email"      varchar(255) NOT NULL UNIQUE,
+        "password"   varchar(255) NOT NULL,
+        "position"   varchar(255),
+        "role"       "role_enum"  NOT NULL DEFAULT 'user',
         "avatar_url" varchar(500),
-        "created_at" timestamp NOT NULL DEFAULT now(),
-        "updated_at" timestamp NOT NULL DEFAULT now(),
+        "created_at" timestamp    NOT NULL DEFAULT now(),
+        "updated_at" timestamp    NOT NULL DEFAULT now(),
         "deleted_at" timestamp,
 
         CONSTRAINT "PK_user_id" PRIMARY KEY ("user_id")
