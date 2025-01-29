b0ec89d90868df016fefaef5f0c16106e138382a
danilPorkchop15 <danilkostin150705@gmail.com>
2025-01-30 02:41:43 +0300

    update: make auth module global

--- packages/backend/src/app/auth/auth.module.ts
+++ packages/backend/src/app/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { PermissionModule } from "@backend/app/permission/permission.module";
-import { Module } from "@nestjs/common";
+import { Global, Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
 
 import { ConfigModule } from "../config/config.module";
@@ -10,6 +10,7 @@ import { AuthController } from "./auth.controller";
 import { AuthGuard } from "./auth.guard";
 import { AuthService } from "./auth.service";
 
+@Global()
 @Module({
   imports: [
     UserModule,
