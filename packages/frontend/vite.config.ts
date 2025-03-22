07329d87eab1c5fd8b8112924bca74e8eb2b065e
Данил Костин <danilkostin150705@gmail.com>
2025-03-23 01:06:03 +0300

    build: set up configs to work with tsyringe and taildwindcss, temporary cdn

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,8 +1,12 @@
 /// <reference types='vitest' />
 import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { esbuildDecorators } from "@anatine/esbuild-decorators";
+import { join } from "path";
 
+// @ts-ignore
+// @ts-ignore
 export default defineConfig({
   root: __dirname,
   cacheDir: "../../node_modules/.vite/packages/frontend",
@@ -17,18 +21,37 @@ export default defineConfig({
     host: "0.0.0.0"
   },
 
+  resolve: {
+    alias: {
+      "@frontend": __dirname + "/src"
+    }
+  },
   css: {
     modules: {
       generateScopedName: "[name]__[local]___[hash:base64:5]"
+    },
+    postcss: "./postcss.config.js"
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        esbuildDecorators({
+          tsx: true,
+          tsconfig: join(__dirname, "tsconfig.app.json")
+        })
+      ]
     }
   },
-  plugins: [react(), nxViteTsPaths()],
 
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
+  plugins: [
+    react({
+      useAtYourOwnRisk_mutateSwcOptions(swcOptions) {
+        // @ts-ignore
+        swcOptions.jsc.parser.decorators = true;
+      }
+    }),
+    nxViteTsPaths()
+  ],
   build: {
     outDir: "../../dist/packages/frontend",
     reportCompressedSize: true,
