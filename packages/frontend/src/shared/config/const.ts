651345b2d6d5b4d2fce9f2b075fea1743af878f7
Danil Kostin <d.kostin@worksolutions.ru>
2025-04-22 18:01:28 +0300

    fix

--- packages/frontend/src/shared/config/const.ts
+++ packages/frontend/src/shared/config/const.ts
@@ -1,4 +1,4 @@
 export const IS_PRODUCTION_MODE: boolean = import.meta.env.MODE === "production";
-export const BASE_API_HOST: string = import.meta.env.VITE_APP_BACKEND || "backend:3000";
+export const BASE_API_HOST: string = import.meta.env.VITE_APP_BACKEND || "http://localhost:3000";
 
 console.info(BASE_API_HOST);
